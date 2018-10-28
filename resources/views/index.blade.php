b785bbfde94f4193cc94df38d808b8137ea39e2b
nikosson <preyforpleasure@gmail.com>
2018-10-29 00:17:39 +0200

    Update readme.md; Update view for all artists

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,16 +12,23 @@
             <p class="lead">Hа этой странице вы можете увидеть всех доступных артистов. Нажмите на ссылку чтобы увидеть их треки</p>
 
             <ul class="list-group">
-                @foreach ($artists as $key => $artist)
-                    <li class="list-group-item artist-item">
-                        <a href="{{ route('artist', ['artist' => $artist->id]) }}">
-                            {{ $key + 1}} {{ $artist->name }}
-                        </a>
-                        @if (!$artist->downloadedTracks->count())
-                            <span class="badge badge-warning">У этого артиста нету загруженых треков</span>
-                        @endif
-                    </li>
-                @endforeach
+
+                @if(count($artists))
+                    @foreach ($artists as $key => $artist)
+                        <li class="list-group-item artist-item">
+                            <a href="{{ route('artist', ['artist' => $artist->id]) }}">
+                                {{ $key + 1}} {{ $artist->name }}
+                            </a>
+                            @if (!$artist->downloadedTracks->count())
+                                <span class="badge badge-warning">У этого артиста нету загруженых треков</span>
+                            @endif
+                        </li>
+                    @endforeach
+                @else
+                    <div class="alert alert-primary" role="alert">
+                        Похоже, что артистов не нашлось :(
+                    </div>
+                @endif
             </ul>
         </div>
 
